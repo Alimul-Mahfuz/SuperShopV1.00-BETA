f835a7725acae8e0cf44e545357d99304e6905a2
Alimul-Mahfuz <alimulmahfuzttushar@gmail.com>
2020-12-15 19:44:50 +0600

    Update BillDetailsDashboard.Designer.cs

--- SuperShopMainV1.00_BETA/SuperShopMainV1.00/BillDetailsDashboard.Designer.cs
+++ SuperShopMainV1.00_BETA/SuperShopMainV1.00/BillDetailsDashboard.Designer.cs
@@ -28,9 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // BillDetailsDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 697);
+            this.MinimumSize = new System.Drawing.Size(868, 736);
+            this.Name = "BillDetailsDashboard";
             this.Text = "BillDetailsDashboard";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
