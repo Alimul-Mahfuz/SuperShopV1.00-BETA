3abdcc5533f5d2ff5258f759d8fc80c0f187cfc3
Alimul-Mahfuz <alimulmahfuzttushar@gmail.com>
2020-12-24 22:38:53 +0600

    Panel added

--- SuperShopMainV1.00_BETA/SuperShopMainV1.00/ui_ux Desingn/SalesmanDashboard.Designer.cs
+++ SuperShopMainV1.00_BETA/SuperShopMainV1.00/ui_ux Desingn/SalesmanDashboard.Designer.cs
@@ -36,7 +36,6 @@
             this.SalesButton = new System.Windows.Forms.Button();
             this.dashboardpicbox = new System.Windows.Forms.PictureBox();
             this.CustomerDashboarplanel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
             this.datetimeviewer = new System.Windows.Forms.TextBox();
             this.salesdashproductid = new System.Windows.Forms.Label();
             this.productidtextbox = new System.Windows.Forms.TextBox();
@@ -59,7 +58,6 @@
             this.proccedtopay = new System.Windows.Forms.Button();
             this.grandtotalcount = new System.Windows.Forms.TextBox();
             this.TotalLabel = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
             this.SalesDashboardPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dashboardpicbox)).BeginInit();
             this.CustomerDashboarplanel1.SuspendLayout();
@@ -157,8 +155,6 @@
             this.CustomerDashboarplanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.CustomerDashboarplanel1.BackColor = System.Drawing.Color.AntiqueWhite;
-            this.CustomerDashboarplanel1.Controls.Add(this.button2);
-            this.CustomerDashboarplanel1.Controls.Add(this.button1);
             this.CustomerDashboarplanel1.Controls.Add(this.datetimeviewer);
             this.CustomerDashboarplanel1.Controls.Add(this.salesdashproductid);
             this.CustomerDashboarplanel1.Controls.Add(this.productidtextbox);
@@ -176,16 +172,6 @@
             this.CustomerDashboarplanel1.Name = "CustomerDashboarplanel1";
             this.CustomerDashboarplanel1.Size = new System.Drawing.Size(657, 168);
             this.CustomerDashboarplanel1.TabIndex = 2;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(304, 119);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(56, 19);
-            this.button1.TabIndex = 24;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // datetimeviewer
             // 
@@ -421,16 +407,6 @@
             this.TotalLabel.TabIndex = 21;
             this.TotalLabel.Text = "Total:";
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(379, 119);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(56, 19);
-            this.button2.TabIndex = 25;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // SalesmanDashboar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -490,7 +466,5 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn QuantityCol;
         private System.Windows.Forms.DataGridViewTextBoxColumn Pricecol;
         private System.Windows.Forms.DataGridViewTextBoxColumn Totalcol;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
     }
 }
