6a873958e089f16a6009120f724e5b717e212754
Alimul-Mahfuz <alimulmahfuzttushar@gmail.com>
2020-12-25 21:43:23 +0600

    payment confirmation form created

---
+++ SuperShopMainV1.00_BETA/SuperShopMainV1.00/ui_ux Desingn/PaymentConfirmationForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperShopMainV1._00.ui_ux_Desingn
+{
+    public partial class PaymentConfirmationForm : Form
+    {
+        public PaymentConfirmationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PaymentConfirmationForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void paytotal_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
