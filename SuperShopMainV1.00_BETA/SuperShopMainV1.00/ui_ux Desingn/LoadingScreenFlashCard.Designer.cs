2d67bd5e8596d2155f4409e5990216d8f162f97a
Alimul-Mahfuz <alimulmahfuzttushar@gmail.com>
2021-01-04 15:31:58 +0600

    Full finalized

--- SuperShopMainV1.00_BETA/SuperShopMainV1.00/ui_ux Desingn/LoadingScreenFlashCard.Designer.cs
+++ SuperShopMainV1.00_BETA/SuperShopMainV1.00/ui_ux Desingn/LoadingScreenFlashCard.Designer.cs
@@ -57,7 +57,7 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.SeaGreen;
             this.label1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(372, 126);
+            this.label1.Location = new System.Drawing.Point(375, 141);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(85, 17);
             this.label1.TabIndex = 1;
